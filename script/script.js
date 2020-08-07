a1553df64a93a74ddee6ae2258657714ec11649a
source <dean-91@mail.ru>
2020-08-07 09:24:20 +0300

    изменени

--- script/script.js
+++ script/script.js
@@ -1,3 +1,5 @@
-document.getElementById('github').onclick = function () {
+let message = function () {
    alert('Изучи git и github');
 };
+
+document.getElementById('github').addEventListener('click', message);
