4890ac1e55ddeb4d7d62b1380f081b67fca7486a
source <dean-91@mail.ru>
2020-08-07 10:21:58 +0300

    сделал дз

---
+++ script/scriptWorkHome.js
@@ -0,0 +1,12 @@
+let money = 'myMoney';
+let income = 'myIncomeDepends';
+let addExpenses = 'addCarCosts';
+let deposit = 'depositInTheBank';
+let mission = 'learnJs';
+let period = 'oneYears';
+
+
+alert("Hello world!");
+
+console.log(money);
+console.log(mission);
